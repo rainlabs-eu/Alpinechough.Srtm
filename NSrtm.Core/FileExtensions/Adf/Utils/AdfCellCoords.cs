2f482d0a0f99b7a115d085146fcd96c650dcc37c
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-08-21 13:51:21 +0200

    Removed unused Equality From Hgt, refacored cellcoords

--- NSrtm.Core/FileExtensions/Adf/Utils/AdfCellCoords.cs
+++ NSrtm.Core/FileExtensions/Adf/Utils/AdfCellCoords.cs
@@ -4,11 +4,11 @@ using NSrtm.Core.Core.Utils;
 
 namespace NSrtm.Core
 {
-    internal struct HgtCellCoords
+    internal struct AdfCellCoords
     {
         private readonly CellCoords _cornerCoords;
 
-        private HgtCellCoords(int lat, int lon)
+        private AdfCellCoords(int lat, int lon)
         {
             _cornerCoords = new CellCoords(lat, lon);
         }
@@ -17,9 +17,9 @@ namespace NSrtm.Core
 
         #region Static Members
 
-        public static HgtCellCoords ForLatLon(double latitude, double longitude)
+        public static AdfCellCoords ForLatLon(double latitude, double longitude)
         {
-            return new HgtCellCoords((int)Math.Floor(latitude), (int)Math.Floor(longitude));
+            return new AdfCellCoords((int)Math.Floor(latitude / 45.0) * 45, (int)Math.Floor(longitude / 45.0) * 45);
         }
 
         #endregion
---
+++ NSrtm.Core/FileExtensions/Utils/CellCoords.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace NSrtm.Core.Core.Utils
+{
+    internal struct CellCoords : IEquatable<CellCoords>
+    {
+        private readonly int _cornerLat;
+        private readonly int _cornerLon;
+
+        internal CellCoords(int cornerLat, int cornerLon)
+        {
+            _cornerLat = cornerLat;
+            _cornerLon = cornerLon;
+        }
+
+        public int LeftUpperCornerLat { get { return _cornerLat; } }
+        public int LeftUpperCornerLon { get { return _cornerLon; } }
+
+        public bool Equals(CellCoords other)
+        {
+            return _cornerLat == other._cornerLat && _cornerLon == other._cornerLon;
+        }
+
+        public override bool Equals([CanBeNull] object obj)
+        {
+            return obj is CellCoords && Equals((CellCoords)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_cornerLat * 397) ^ _cornerLon;
+            }
+        }
+
+        public static bool operator ==(CellCoords left, CellCoords right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CellCoords left, CellCoords right)
+        {
+            return !left.Equals(right);
+        }
+
+        public string ToBaseName()
+        {
+            return String.Format("{0}{1:D2}{2}{3:D3}",
+                                 _cornerLat < 0 ? "S" : "N",
+                                 Math.Abs(_cornerLat),
+                                 _cornerLon < 0 ? "W" : "E",
+                                 Math.Abs(_cornerLon));
+        }
+    }
+}
