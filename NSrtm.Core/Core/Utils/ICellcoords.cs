00ebb7770683238b27b2c44be340e42e63a2d253
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-08-21 12:44:12 +0200

    Separated common part (CellCoords)
    []

---
+++ NSrtm.Core/Core/Utils/ICellcoords.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace NSrtm.Core.Core.Utils
+{
+    public interface ICellCoords
+    {
+        int LeftUpperCornerLat { get; }
+        int LeftUpperCornerLon { get; }
+        ICellCoords ForLatLon(double latitude, double longitude);
+        string ToBaseName();
+    }
+}
